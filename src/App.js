8f6ef700d6ee19ae9d4df97e181505988fcf7fd8
Kingstone Chandaengerwa <h240176b@hit.ac.zw>
2026-04-07 12:54:40 +0200

    Fixing MapView

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from"./pages/Home";
 import Signup from "./pages/Signup";
 import Login from "./pages/Login";
-import MapView from "./components/MapView";
 import ProtectedRoute from "./components/ProtectedRoute";
 import AddProperty from "./pages/AddProperty";
 import Dashboard from "./pages/Dashboard";
@@ -18,7 +17,7 @@ function App() {
           <Route path="/Signup" element={<Signup />} />
           <Route path="/add-property" element={<AddProperty />} />
           <Route path="/simulate-payment/:bookingId" element={<SimulatePayment />} />
-          // App.js
+          
           <Route
             path="/dashboard"
             element={
